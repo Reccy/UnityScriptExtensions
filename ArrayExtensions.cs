ca18bc00c4c6614b7cf6b5a5e3fcec5ed76f6d2a
Aaron Meaney <aaronmeaney@hotmail.com>
2021-10-20 15:38:25 +0100

    Add ArrayExtensions

---
+++ ArrayExtensions.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Reccy.ScriptExtensions
+{
+    public static class ArrayExtensions
+    {
+        public static T SelectRandom<T>(this T[] array)
+        {
+            var i = Random.Range(0, array.Length);
+            return array[i];
+        }
+    }
+}
