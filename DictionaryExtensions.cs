ebfecdd7254f8b5977eba4391c3fa753805c5d1d
Aaron Meaney <aaronmeaney@hotmail.com>
2021-10-20 20:44:21 +0100

    DictionaryExtensions added

---
+++ DictionaryExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace Reccy.ScriptExtensions
+{
+    public static class DictionaryExtensions
+    {
+        /// <summary>
+        /// Returns the key with the smallest value in the dictionary.
+        /// </summary>
+        public static T GetSmallest<T>(this Dictionary<T, float> dict)
+        {
+            T smallestKey = default(T);
+            float smallestValue = float.MaxValue;
+
+            foreach (KeyValuePair<T, float> kvp in dict)
+            {
+                T k = kvp.Key;
+                float v = kvp.Value;
+
+                if (v < smallestValue)
+                {
+                    smallestKey = k;
+                    smallestValue = v;
+                }
+            }
+
+            return smallestKey;
+        }
+    }
+}
