a19609894422965464959a7488d5acf044d1b737
Aaron Meaney <aaronmeaney@hotmail.com>
2021-10-20 18:54:27 +0100

    Add StackExtensions

---
+++ StackExtensions.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace Reccy.ScriptExtensions
+{
+    public static class StackExtensions
+    {
+        public static List<T> ToList<T>(this Stack<T> stack)
+        {
+            return new List<T>(stack.ToArray());
+        }
+    }
+}
